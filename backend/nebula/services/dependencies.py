290d58fed30ab4a3ee5b1160ed9ad0a9ae007a35
Igor Beschastnov <beschastnovigor@gmail.com>
2020-02-15 13:08:16 +0300

    Remove sql, add mongo.

--- backend/nebula/services/dependencies.py
+++ backend/nebula/services/dependencies.py
@@ -1,14 +1,5 @@
 from typing import Generator
 
-from database import Session
-
 
 # FastAPI Dependency
-def get_db() -> Generator[Session, None, None]:
-    db = None
-    try:
-        db = Session()
-        yield db
-    finally:
-        if db:
-            db.close()
+# TODO: get_db

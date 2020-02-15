4d636668e5337f12b59a8d381ec594e127af227d
Anastasia Kogteva <kogteva.nastya@gmail.com>
2020-02-15 17:12:37 +0300

    separate main page to components

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Line } from 'shared/base/line';
+import { UploadZone } from 'app/uploadZone';
+import { ProgressControl } from 'app/progressControl';
+import { DownloadZone } from 'app/downloadZone';
+import { ProcessControl } from 'app/processControl';
+
+import './app.scss';
 
 export const App: React.FC = () => {
   return (
-    <Line vertical>
-      <h1>May The Force Be With You</h1>
+    <Line justifyContent="between" className="app">
+      <Line vertical className="leftPanel">
+        <ProgressControl />
+        <UploadZone />
+        <DownloadZone />
+      </Line>
+      <Line className="rightPanel">
+        <ProcessControl />
+      </Line>
     </Line>
   );
 };
